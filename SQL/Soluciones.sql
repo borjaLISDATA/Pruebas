2c61cb3362bff25d49957094a029b512b3e28fe6
Borja Fernandez <borja.fernandez@lisdatasolutions.com>
2025-07-18 10:43:20 +0200

    SQL Corregido

--- SQL/Soluciones.sql
+++ SQL/Soluciones.sql
@@ -16,30 +16,32 @@ select c.first_name, c.last_name from customer c  where c.customer_id not in (se
 -- Para pagar las comisiones por el empleado 345, necesitamos las ventas del año 2022 realizadas por dicho empleado desglosadas por meses.
 -- ¿Podrías obtener dicha información?
 
-select s.first_name, s.last_name, count(p.payment_id) as total_de_ventas , sum(p.amount) as cantidad_total, to_char(p.payment_date,'mon/YYYY') as fecha from staff s 
-join payment p on s.staff_id = p.staff_id 
-where s.staff_id = 345 and extract(year from p.payment_date) = 2022
-group by(s.staff_id, to_char(p.payment_date,'mon/YYYY'));
+select staff_id, count(payment_id) as total_de_ventas , sum(amount) as cantidad_total, to_char(payment_date,'mon') as fecha 
+FROM payment
+where staff_id = 345 and extract(year from payment_date) = 2022
+group by(staff_id, to_char(payment_date,'mon'));
 
 
 -- Obtener un listado en el que se muestren el total acumulado de los pagos realizados por cada cliente hasta la fecha de cada uno de los pagos.
 -- Es decir, en cada fila de la consulta debes mostrar cada uno de los pagos y una columna adicional con la cantidad total pagada por el cliente
 -- (no mezclar pagos de otros clientes), hasta la fecha del pago actual, incluyendo este último.
 
-create or replace function total_cliente_fecha(cliente_id int, fecha timestamp) returns float as
-$$select sum(amount) as cantidad from payment where customer_id = cliente_id and payment_date between '2000-01-01 00:00:00' and fecha$$
-language sql;
-
-
-select c.first_name, c.last_name, p.payment_date, p.amount as importe , total_cliente_fecha(c.customer_id, p.payment_date) as total_gastado
+select c.first_name, c.last_name, p.payment_date, p.amount as importe, sum(amount) OVER(partition by c.customer_id order by p.payment_date asc) as total_gastado
 from customer c join payment p on c.customer_id =p.customer_id order by(c.first_name, c.last_name, p.payment_date);
 
 
 -- Usando una consulta SQL, obtén un listado que contenga el número de películas que existen en el sistema para cada pareja de categoría
 -- (identificada por su nombre) e idioma (también por su nombre) existentes.
 
-select c.name, l."name", sum(f.film_id) as total_pelis
-from language l join film f on l.language_id =f.film_id 
+select c.name as Categoria, l.name as Idioma, count(f.film_id) as Numpelis
+from language l join film f on l.language_id =f.language_id 
 				join film_category fc on f.film_id = fc.film_id 
 				join category c on fc.category_id =c.category_id
 group by(c.category_id,l.language_id);
+
+-- Para ver si la suma de los valores NumPelis da el total de peliculas hay que ver si existe alguna pelicula que no tenga asignada una categoria o idioma
+select * from film f where f.film_id 
+not in(select film_id from film_category) or
+f.language_id is null;
+-- La lista es vacia por lo que todas las pelis tienen un idioma asignado y una categoria, lo que demuestra que la suma de los valores NumPelis es el total de peliculas
+
